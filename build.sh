f69926030c16bf5ac1451d7e93b8dc2d90a264a7
Shahasil <shahasil54@gmail.com>
2026-03-12 09:34:18 +0530

    Update build script to use virtual environmentFix build script for Vercel: use virtualenv to avoid managed environment error
    Set up a virtual environment and install dependencies.

--- build.sh
+++ build.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 echo "Building project..."
-python3 -m pip install -r requirements.txt
+python3 -m venv venv
+source venv/bin/activate
+pip install -r requirements.txt
 python3 manage.py collectstatic --noinput --clear
 python3 manage.py migrate
