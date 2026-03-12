5271c4a35677bec3cafcb2b41261770ae821a5f4
Shahasil <shahasil54@gmail.com>
2026-03-12 10:21:25 +0530

    Fix script to ensure failure stops execution

--- build.sh
+++ build.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
+set -e
 echo "Building project..."
-python3 -m venv venv
-source venv/bin/activate
 pip install -r requirements.txt
 python3 manage.py collectstatic --noinput --clear
 python3 manage.py migrate
