cff61c5ecbeb70c6f67e93cd67bbe35c36371f18
Shahasil <shahasil54@gmail.com>
2026-03-12 10:32:32 +0530

    Modify pip install command to include --break-system-packages

--- build.sh
+++ build.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -e
 echo "Building project..."
-pip install -r requirements.txt
+pip install -r requirements.txt --break-system-packages
 python3 manage.py collectstatic --noinput --clear
 python3 manage.py migrate
 python3 create_superuser.py
